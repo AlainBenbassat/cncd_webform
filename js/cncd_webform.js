a98b2821ee38e502cc1d5f2822ea09cfc7f85c41
Alain Benbassat <alain@businessandcode.eu>
2018-05-25 16:11:12 +0200

    bugfix

--- js/cncd_webform.js
+++ js/cncd_webform.js
@@ -1,55 +1,56 @@
-function cncd_webform_provinceSelect() {
-  // get the name of the selected province
-  var selectID = this.id;
-  var selectedProvince = jQuery('#' + selectID + ' option:selected').text();
-
-  // find the matching tag label
-  var labelContent = "";
-  switch (selectedProvince) {
-    case "Antwerpen":
-      labelContent = "- - - Flandre ";
-      break;
-    case "Brabant Wallon":
-      labelContent = "- - - Brabant Wallon ";
-      break;
-    case "Brussels":
-      labelContent = "";
-      break;
-    case "Hainaut":
-      labelContent = "";
-      break;
-    case "Liege":
-      labelContent = "";
-      break;
-    case "Limburg":
-      labelContent = "";
-      break;
-    case "Luxembourg":
-      labelContent = "";
-      break;
-    case "Namur":
-      labelContent = "";
-      break;
-    case "Oost-Vlaanderen":
-      labelContent = "";
-      break;
-    case "West-Vlaanderen":
-      labelContent = "";
-      break;
-    case "Vlaams-Brabant":
-      labelContent = "";
-  }
-
-  // find the label contains the text
-  var checkboxID = jQuery("label:contains('" + labelContent + "'").attr('for');
-  if (checkboxID) {
-    jQuery('#' + checkboxID).prop("checked");
-  }
-}
-
 jQuery(document).ready(
     function() {
       // check if we have a select which name ends with province-id, and add hook
-      jQuery("select[id$='province-id']").on('change', cncd_webform_provinceSelect);
+      jQuery("select[id$='province-id']").on('change',
+          function() {
+            // get the name of the selected province
+            var selectID = this.id;
+            var selectedProvince = jQuery('#' + selectID + ' option:selected').text();
+
+            // find the matching tag label
+            var labelContent = "";
+            switch (selectedProvince) {
+              case "Antwerpen":
+                labelContent = "- - - Flandre ";
+                break;
+              case "Brabant Wallon":
+                labelContent = "- - - Brabant Wallon ";
+                break;
+              case "Brussels":
+                labelContent = "";
+                break;
+              case "Hainaut":
+                labelContent = "- - - Hainaut ";
+                break;
+              case "Liege":
+                labelContent = "- - - Liège ";
+                break;
+              case "Limburg":
+                labelContent = "- - - Flandre ";
+                break;
+              case "Luxembourg":
+                labelContent = "- - - Luxembourg ";
+                break;
+              case "Namur":
+                labelContent = "- - - Namur ";
+                break;
+              case "Oost-Vlaanderen":
+                labelContent = "- - - Flandre ";
+                break;
+              case "West-Vlaanderen":
+                labelContent = "- - - Flandre ";
+                break;
+              case "Vlaams-Brabant":
+                labelContent = "- - - Flandre ";
+                break;
+            }
+
+            // find the label contains the text
+            var checkboxID = jQuery("label:contains('" + labelContent + "')").attr('for');
+            if (checkboxID) {
+              jQuery('#' + checkboxID).attr("checked", "checked");
+            }
+          }
+      );
     }
 );
